c02d3fa4c84509c6fad2f285b7ccc64593889ca7
simranjit <simranjit.singh@um.es>
2022-11-23 16:58:04 +0100

    404 not found

--- proc.c
+++ proc.c
@@ -225,7 +225,7 @@ fork(void)
 // An exited process remains in the zombie state
 // until its parent calls wait() to find out it exited.
 void
-exit(void)
+exit(int status_code)
 {
   struct proc *curproc = myproc();
   struct proc *p;
@@ -273,7 +273,7 @@ exit(void)
 // Wait for a child process to exit and return its pid.
 // Return -1 if this process has no children.
 int
-wait(void)
+wait(int * status_code_ptr)
 {
   struct proc *p;
   int havekids, pid;
