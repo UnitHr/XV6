29556347447fa8b1dfe3373562ee25b495558455
simranjit <simranjit.singh@um.es>
2022-12-12 19:33:16 +0100

    end

--- proc.c
+++ proc.c
@@ -334,7 +334,7 @@ wait(int * status_code_ptr)
 
 //Add to the scheduler queue
 /* MUST BE CALLED WHILE LOCKED */
-void addToSchedulerQueue(struct proc *p, struct proc_table * ptable ){
+void addToSchedulerQueue(struct proc *p, struct proc_table * ptable ){/*
   int index;
   int rand;
   if( p->prio == HI_PRIO ){
@@ -351,6 +351,7 @@ void addToSchedulerQueue(struct proc *p, struct proc_table * ptable ){
   }
   ptable->running_queue[index] = ptable->running_queue[rand];
   ptable->running_queue[rand] = p;
+  */
   p->state = RUNNABLE;
 }
 
@@ -363,7 +364,7 @@ void addToSchedulerQueue(struct proc *p, struct proc_table * ptable ){
 //  - swtch to start running that process
 //  - eventually that process transfers control
 //      via swtch back to the scheduler.
-
+/*
 void
 scheduler(void)
 {
@@ -410,6 +411,53 @@ scheduler(void)
     }
 
     release(&ptable.lock);
+  }
+}
+*/
+
+void
+scheduler(void)
+{
+  struct proc *p;
+  struct cpu *c = mycpu();
+  c->proc = 0;
+
+  for(;;){
+    // Enable interrupts on this processor.
+    sti();
+
+    // Loop over process table looking for process to run.
+    acquire(&ptable.lock);
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->state != RUNNABLE)
+        continue;
+
+      //Search for the process with the highest priority
+      struct proc * hp = p;
+      while( hp < &ptable.proc[NPROC] ){
+        if( hp->state == RUNNABLE && hp->prio == HI_PRIO ){
+          p = hp;
+          break; //Found the process with the highest priority
+        }
+        hp++;
+      }
+
+      // Switch to chosen process.  It is the process's job
+      // to release ptable.lock and then reacquire it
+      // before jumping back to us.
+      c->proc = p;
+      switchuvm(p);
+      p->state = RUNNING;
+
+      swtch(&(c->scheduler), p->context);
+      switchkvm();
+
+      // Process is done running for now.
+      // It should have changed its p->state before coming back.
+      c->proc = 0;
+    }
+    release(&ptable.lock);
+
   }
 }
 
